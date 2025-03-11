f3174b4fec7ec82165d79eab3db5c833bfe439d8
Queri48 <hounsaq@gmail.com>
2025-03-11 14:03:43 +0100

    tp1

--- resources/views/admin/articles/edit.blade.php
+++ resources/views/admin/articles/edit.blade.php
@@ -42,9 +42,19 @@
             </div>
             <div class="row gy-4 gy-xl-5 p-4 p-xl-5">
                 <div class="col-12">
+                    <label for="titre" class="form-label">Titre de la categorie<span class="text-danger">*</span></label>
+                    <input type="text" class="form-control" id="categorie_titre" name="categorie_titre"
+                        value="{{ old('categorie_titre', $article->categorie->titre) }}" required>
+                    @error('categorie_titre')
+                        <div class="text-danger">{{ $message }}</div>
+                    @enderror
+                </div>
+            </div>
+            <div class="row gy-4 gy-xl-5 p-4 p-xl-5">
+                <div class="col-12">
                     <label for="categorie_id" class="form-label">Categorie <span class="text-danger">*</span></label>
                     <select name="article_categorie_id" class="form-control form-select" id="article_categorie_id">
-                        <option value="" selected>Selectionner une catégorie</option>
+                        <option value="{{ old('article_categorie_id', $article->categorie_id) }}" selected>Selectionner une catégorie</option>
                         @foreach ($categories as $categorie)
                             <option value="{{ $categorie->id }}">{{ $categorie->titre }}</option>
                         @endforeach
@@ -54,20 +64,10 @@
                     @enderror
                 </div>
             </div>
-            <div class="row gy-4 gy-xl-5 p-4 p-xl-5">
-                <div class="col-12">
-                    <label for="titre" class="form-label">Titre de la categorie<span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="categorie_titre" name="categorie_titre"
-                        value="{{ old('categorie_titre', $article->categorie->titre) }}" required>
-                    @error('categorie_titre')
-                        <div class="text-danger">{{ $message }}</div>
-                    @enderror
-
-                </div>
-            </div>
             <div class="col-12">
                 <label for="description" class="form-label">Description <span class="text-danger">*</span></label>
-                <textarea class="form-control" id="article_description" name="article_description" rows="3" cols="40" required></textarea>
+                <textarea class="form-control" id="article_description" name="article_description" rows="3" cols="40"
+                value="{{ old('article_description', $article->description) }}" required></textarea>
                 @error('article_description')
                     <div class="text-danger">{{ $message }}</div>
                 @enderror
@@ -75,7 +75,8 @@
             <div class="row gy-4 gy-xl-5 p-4 p-xl-5">
                 <div class="col-12">
                     <label for="tags" class="form-label">Tags <span class="text-danger">*</span></label>
-                    <input type="text" class="form-control" id="article_tags" name="article_tags" value="" required>
+                    <input type="text" class="form-control" id="article_tags" name="article_tags"
+                    value="{{ old('article_tags', $article->tags) }}" required>
                     @error('tags')
                         <div class="text-danger">{{ $message }}</div>
                     @enderror
